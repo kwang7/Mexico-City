4e058ca04ee81d3ef8a0d3d2354c03e417c9595c
Jake Zaia <jzaia@stuy.edu>
2017-04-21 08:39:37 -0400

    Started on HelpDesk

--- HelpDesk.java
+++ HelpDesk.java
@@ -1,5 +1,16 @@
 public class HelpDesk {
+    
+    private ArrayPriorityQueue<Ticket> _tickets;
+    
+    public HelpDesk(){
+	_tickets = new ArrayPriorityQueue<Ticket>();
+    }
+    
+    public void openTicket(int id){
+	_tickets.add(new Ticket("Bob", "no,", id, 0));
+    }
 
-
-    public static void main(String [] args){}
+    public static void main(String [] args){
+	openTicket(0);
+    }
 }
