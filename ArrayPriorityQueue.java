8baf56e9b0366aff5fa7b648e682fd849e5f9def
Jake Zaia <jzaia@stuy.edu>
2017-04-21 08:17:53 -0400

    Fixed ArrayPriorityQueue to work with generic typing

--- ArrayPriorityQueue.java
+++ ArrayPriorityQueue.java
@@ -1,25 +1,25 @@
-// Jake Zaia
+// Team Mexico City: Jasper Cheung, Kelly Wang, Jake Zaia
 // APCS2 p1
-// HW #32: Getting Past the Velvet Rope
-// 2017-04-19
+// LAB #03: RUVIP?
+// 2017-04-24
 
 import java.util.ArrayList;
 
-public class ArrayPriorityQueue implements PriorityQueue {
-    ArrayList<Comparable> _data;
+public class ArrayPriorityQueue<T> implements PriorityQueue<T> {
+    ArrayList<T> _data;
 
     public ArrayPriorityQueue(){
-	_data = new ArrayList<Comparable>();
+	_data = new ArrayList<T>();
     }
 
-    //Adds an item to this priority queue.
-    public void add(Comparable x){
+    //Adds an item to this priority queue. Type MUST be Comparable
+    public void add(T x){
 	if (isEmpty())
 	    _data.add(x);
 	else {
 	    int i = 0;
 	    while (i < _data.size()){
-		if (_data.get(i).compareTo(x) >= 0){
+		if (((Comparable) _data.get(i)).compareTo((Comparable) x) < 0){
 		    _data.add(i,x);
 		    return;
 		}
@@ -35,17 +35,17 @@ public class ArrayPriorityQueue implements PriorityQueue {
     }
     
     //Returns the smallest item stored in this priority queue without removing it.
-    public Comparable peekMin(){
+    public T peekMin(){
 	return _data.get(_data.size() - 1);
     }
  
     //Removes and returns the smallest item stored in this priority queue.
-    public Comparable removeMin(){
+    public T removeMin(){
 	return _data.remove(_data.size() - 1);
     }
     
     public static void main(String [] args){
-	ArrayPriorityQueue printer = new ArrayPriorityQueue();
+	ArrayPriorityQueue<Integer> printer = new ArrayPriorityQueue<>();
 	printer.add(1);
 	printer.add(2);
 	printer.add(3);
--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,59 +1,20 @@
-// Jake Zaia
+// Team Mexico City: Jasper Cheung, Kelly Wang, Jake Zaia
 // APCS2 p1
-// HW #32: Getting Past the Velvet Rope
-// 2017-04-19
+// LAB #03: RUVIP?
+// 2017-04-24
 
-import java.util.ArrayList;
-
-public class ArrayPriorityQueue implements PriorityQueue {
-    ArrayList<Comparable> _data;
-
-    public ArrayPriorityQueue(){
-	_data = new ArrayList<Comparable>();
-    }
+public interface PriorityQueue<T> {
 
     //Adds an item to this priority queue.
-    public void add(Comparable x){
-	if (isEmpty())
-	    _data.add(x);
-	else {
-	    int i = 0;
-	    while (i < _data.size()){
-		if (_data.get(i).compareTo(x) >= 0){
-		    _data.add(i,x);
-		    return;
-		}
-		i++;
-	    }
-	    _data.add(x);
-	} 
-    }
+    public void add(T x);
     
     //Returns true if this stack is empty, otherwise returns false.
-    public boolean isEmpty(){
-	return _data.size() == 0;
-    }
+    public boolean isEmpty();
     
     //Returns the smallest item stored in this priority queue without removing it.
-    public Comparable peekMin(){
-	return _data.get(_data.size() - 1);
-    }
+    public T peekMin();
  
     //Removes and returns the smallest item stored in this priority queue.
-    public Comparable removeMin(){
-	return _data.remove(_data.size() - 1);
-    }
+    public T removeMin();
     
-    public static void main(String [] args){
-	ArrayPriorityQueue printer = new ArrayPriorityQueue();
-	printer.add(1);
-	printer.add(2);
-	printer.add(3);
-	printer.add(2);
-	printer.add(1);
-	while (! printer.isEmpty()) {
-	    System.out.println(printer.removeMin());
-	}
-    }
-
 }
