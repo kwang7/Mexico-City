71762d10844ff1c10c0aebffdc9c276685d33174
Kelly Wang <kwang7@stuy.edu>
2017-04-23 19:25:42 -0400

    yes

--- HelpDesk.java
+++ HelpDesk.java
@@ -1,4 +1,5 @@
 import cs1.Keyboard;
+
 public class HelpDesk {
     
     private ArrayPriorityQueue<Ticket> _tickets;
@@ -17,6 +18,27 @@ public class HelpDesk {
 	System.out.print(start);
 	String name = Keyboard.readString();
 	
-    }
+	System.out.println("What's your problem?? \n1. My computer won't turn on \n2. My computer is slow/frozen \n3.My internet isn't working");
+
+	String desc;
+	int prio;
+	if ( d == 1 ) {
+	    desc = "My computer won't turn on";
+	    prio = 1;
+	}
+	else if ( d == 2 ) {
+	    desc = "My computer is slow/frozen";
+	    prio = 2;
+	}
+	else if ( d == 3 ) {
+	    desc = "My internet isn't working";
+	    prio = 3;
+	}
+	else { 
+	    System.out.println("that is not a real problem");
+	    desc = "no";
+	    prio = 9999999;
+	}
 	
+    }
 }
